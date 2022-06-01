bf9a35e404670f8eebd3f3d3b17c12f3b303a2bb
Justin Quinn <jpq3366@gmail.com>
2022-06-01 15:40:20 -0400

    Added cors module and process.env to index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const express= require('express');
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
 
 
 let notes = [
@@ -92,5 +94,6 @@ app.post('/api/notes', (req,res) => {
 })
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
